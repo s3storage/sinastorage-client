60abf489a6f3cc9c2e9af43fe0faa80c470a2541
dengyanhuihappy@gmail.com <dengyanhuihappy@gmail.com@264ec00a-7f65-a066-188c-dedd9fa28a74>
2012-05-18 06:54:23 +0000

    add a example copyBetweenProject.php
    git-svn-id: https://sinastorage-clients.googlecode.com/svn/trunk@37 264ec00a-7f65-a066-188c-dedd9fa28a74

---
+++ php/examples/copyBetweenProject.php
@@ -0,0 +1,17 @@
+<?php
+require("../SinaStorageService.php");
+require_once( "conf.php" );
+
+$o = SinaStorageService::getInstance($project, $accesskey, $secretkey);
+
+
+
+//可自定义配置的CURLOPT
+$o->setCURLOPTs(array(CURLOPT_VERBOSE=>1));
+$o->setRequestHeaders(array("fortest"=>1));
+
+//设置使用验证方式
+$o->setAuth(true);
+
+$rc = $o->copyFile("1123.html", $src_projec_name , "123.html", $result);
+echo $result;
